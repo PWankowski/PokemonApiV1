6930d62959dfb35844452002d9e0084649882b56
Paweł <pawel.wankowski@wp.pl>
2021-10-24 18:30:53 +0200

    feature(paa): add pokemon list init loader

---
+++ src/main/java/com/project/PokemonApplicationV1/pokemonlist/PokemonListLoader.java
@@ -0,0 +1,21 @@
+package com.project.PokemonApplicationV1.pokemonlist;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PokemonListLoader implements CommandLineRunner {
+
+    private final PokemonListService pokemonListService;
+
+    public PokemonListLoader(PokemonListService pokemonListService) {
+        this.pokemonListService = pokemonListService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        pokemonListService.getPokemonList();
+
+    }
+}
