122e9f392e9504338970b4ef9531e36961361f4f
Paweł <pawel.wankowski@wp.pl>
2021-11-04 11:47:41 +0100

    feature(paa): add  heroku url to properties

--- src/main/java/com/project/PokemonApplicationV1/pokemonlist/PokemonListService.java
+++ src/main/java/com/project/PokemonApplicationV1/pokemonlist/PokemonListService.java
@@ -3,6 +3,7 @@ package com.project.PokemonApplicationV1.pokemonlist;
 
 import com.project.PokemonApplicationV1.pokemondetails.PokemonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,9 @@ import java.util.stream.Collectors;
 
 @Service
 public class PokemonListService {
-    private static final String ENDPOINT_URL = "https://pokemonapplication-v1.herokuapp.com/pokemon/list?limit=%d&offset=%d";
+    private static final String ENDPOINT_URL = "pokemon/list?limit=%d&offset=%d";
+    @Value("${herokupokeapi.url}")
+    private  String baseUrl;
 
     private final PokemonListNetworkRepository pokemonListNetworkRepository;
     private final PokemonRepository pokemonRepository;
@@ -70,17 +73,20 @@ public class PokemonListService {
                return   pokemonListItemDto.toEntity(pokemonDetails);
                       }).collect(Collectors.toList());
 
+
+
         long count = pokemonRepository.count();
         boolean hasPrev = offset !=0;
         boolean hasNext = (count - ((offset*limit)+limit)) > 0;
         String next = null;
         String prev = null;
+        String url = baseUrl+ENDPOINT_URL;
 
         if(hasPrev){
-            prev = String.format(ENDPOINT_URL, limit, offset -1);
+            prev = String.format(url, limit, offset -1);
         }
         if(hasNext){
-            next = String.format(ENDPOINT_URL, limit, offset +1);
+            next = String.format(url, limit, offset +1);
         }
 
         return new PokemonListEnvelop(count,next,prev,pokemonListItems);
